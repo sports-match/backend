3857c3468bcaa516fd1d69bcf6823e0936a07aa0
chanhengseang <chanheng.seang@mbanq.com>
2025-06-23 23:30:24 -0700

    Closes #77

--- eladmin-system/src/main/java/com/srr/player/repository/TeamRepository.java
+++ eladmin-system/src/main/java/com/srr/player/repository/TeamRepository.java
@@ -10,28 +10,33 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 /**
-* @website https://eladmin.vip
-* @author Chanheng
-* @date 2025-05-25
-**/
+ * @author Chanheng
+ * @date 2025-05-25
+ **/
 public interface TeamRepository extends JpaRepository<Team, Long>, JpaSpecificationExecutor<Team> {
     /**
      * Find all teams for a specific event
+     *
      * @param eventId ID of the event
      * @return List of teams
      */
     @Query("SELECT t FROM Team t WHERE t.event.id = :eventId")
     List<Team> findByEventId(@Param("eventId") Long eventId);
-    
+
     /**
      * Find all teams for a specific event (alias for findByEventId)
+     *
      * @param eventId ID of the event
      * @return List of teams
      */
     List<Team> findAllByEventId(Long eventId);
 
+    @Query("select true from team_player tp join team t on tp.team_id where t.event_id = :eventId and tp.player_id = :playerId")
+    Boolean checkIsJoined(@Param("eventId") Long eventId, @Param("playerId") Long playerId);
+
     /**
      * Delete all teams for a specific event.
+     *
      * @param eventId The event ID.
      */
     @Modifying
