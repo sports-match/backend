4e4be242d9392aa168d07ac8acf921aad7b37f23
chanhengseang <chanheng.seang@mbanq.com>
2025-06-25 22:34:26 -0700

    Fix mapper

--- eladmin-system/src/main/java/com/srr/player/dto/PlayerDetailsDto.java
+++ eladmin-system/src/main/java/com/srr/player/dto/PlayerDetailsDto.java
@@ -1,12 +1,12 @@
 package com.srr.player.dto;
 
-import com.srr.enumeration.Format;
-
-import java.util.List;
-import java.util.Map;
-
 public class PlayerDetailsDto {
     private PlayerDto player;
-    private Map<PlayerSportRatingDto, List<RatingHistoryDto>> sportRatings;
-    private Map<Format, Integer> eventJoined;
+    private Integer singleEventRating;
+    private Integer DoubleEventRating;
+    private Integer singleEventRatingChanges;
+    private Integer DoubleEventRatingChanges;
+    private Integer totalEvent;
+
+
 }
